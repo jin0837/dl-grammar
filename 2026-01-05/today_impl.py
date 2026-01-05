708977713ebc7fe35a5175d195ac9fd248ef72c7
jin0837 <0zg3101878r417v@au.com>
2026-01-05 23:58:54 +0900

    Implement numerical gradient function

---
+++ 2026-01-05/today_impl.py
@@ -0,0 +1,19 @@
+#gradient
+def numerical_gradient(f,x):
+  h = 1e-4 #0.0001
+  grad = np.zero_like(x)
+
+  for idx in range(x.size):
+    tmp_val = x[idx]
+    #f(x+h)
+    x[idx] = tmp_val + h
+    fxh1 = f(x)
+
+    #f(x-h)
+    x[idx] = tmp_val - h
+    fxh2 = f(x)
+
+    grad[idx] = (fxh1 - fxh2) / (2*h)
+    x[idx] = tmp_val
+
+  return grad
