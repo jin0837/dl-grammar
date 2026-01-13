bc2659c5e4b0ff5bf01fca66d59583def7c5e632
jin0837 <0zg3101878r417v@au.com>
2026-01-13 16:21:51 +0900

    Refactor TwoLayerNet initialization and methods

--- 2026-01-13/2026-01-13.py
+++ 2026-01-13/2026-01-13.py
@@ -1,25 +1,25 @@
 class TwoLayerNet:
   def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
-    #重みの初期化
-    self.params = {}
-    self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
-    self.params['b1'] = np.zeros(hidden_size)
-    self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
-    self.params['b2'] = np.zeros(output_size)
-
-    #レイヤの作成
-    self.layers = OrderDict()
-    self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
-    self.layers['Relu1'] = Relu()
-    self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
-
-    self.lastLayer = SoftmaxWithLoss()
+      #重みの初期化
+      self.params = {}
+      self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+      self.params['b1'] = np.zeros(hidden_size)
+      self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+      self.params['b2'] = np.zeros(output_size)
+  
+      #レイヤの作成
+      self.layers = OrderDict()
+      self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+      self.layers['Relu1'] = Relu()
+      self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+  
+      self.lastLayer = SoftmaxWithLoss()
 
   def predict(self, x):
-    for layer in self.layers.values():
-      x = layer.forward(x)
-  
-    return x
+      for layer in self.layers.values():
+        x = layer.forward(x)
+    
+      return x
 
   #xが入力でtが正解ラベル
   def loss(self, x ,t):
@@ -27,11 +27,28 @@ class TwoLayerNet:
       return self.lastLayer.forward(y, t)
 
   def accuracy(self, x, t):
-    y = self.predict(x)
-    y = np.argnax(y, axis=1)
-    if t.ndim = != 1 : t = np.argmax(t, axis=1)
+      y = self.predict(x)
+      y = np.argnax(y, axis=1)
+      if t.ndim = != 1 : t = np.argmax(t, axis=1)
+  
+      accuracy = np.sum(y == t) / float(x.shape[0])
+      return accuracy
 
-    
+  def numerical_gradient(self, x, t):
+      loss_W = lambda W: self.loss(x, t)
+  
+      grads = {}
+      grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+      grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+      grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+      grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+      return grads
+
+  def gradient(self, x, t):
+      #forward
     
 
+
+   
 
