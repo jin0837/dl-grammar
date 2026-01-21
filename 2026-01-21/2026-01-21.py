52752aec81ee79833c790853d21326abf6ee4805
jin0837 <0zg3101878r417v@au.com>
2026-01-21 23:42:19 +0900

    Add Dropout class with initialization and forward method

---
+++ 2026-01-21/2026-01-21.py
@@ -0,0 +1,8 @@
+class Dropout:
+  def __init__(self, dropout_ratio=0.5):
+      self.dropout_ratio = dropout_ratio
+      self.mask = None
+
+  def forward(self, x, train_flg=True):
+      if 
+    
